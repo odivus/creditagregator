04611f99771efa08891f2729f23aac6e9eed4b9a
odivus <odivus@gmail.com>
2020-11-19 22:30:11 +0300

    Add reduxjs toolkit

---
+++ data/goods.js
@@ -0,0 +1,94 @@
+export default {
+  Категория: {
+    Смартфоны: {
+      Бренд: {
+        Apple: [
+          {
+            Модель: "iPhone Max",
+            price: 199500,
+          },
+          {
+            Модель: "iPhone MinMax",
+            price: 298200,
+          },
+          {
+            Модель: "iPhone Mini",
+            price: 99000,
+          },
+        ],
+
+        Samsung: [
+          {
+            Модель: "A21",
+            price: 20200,
+          },
+          {
+            Модель: "M12",
+            price: 30250,
+          },
+          {
+            Модель: "G121",
+            price: 88485,
+          },
+        ],
+
+        Huawei: [
+          {
+            Модель: "Nova 55-X",
+            price: 1255,
+          },
+          {
+            Модель: "Maga-21",
+            price: 2255,
+          },
+          {
+            Модель: "Laja-Yxi",
+            price: 5459,
+          },
+        ],
+      },
+    },
+
+    Ноутбуки: {
+      Бренд: {
+        Apple: [
+          {
+            Модель: "MacBook Pro 13",
+            price: 220000,
+          },
+          {
+            Модель: "MacCook Air",
+            price: 120000,
+          },
+          {
+            Модель: "Mac mini",
+            price: 140000,
+          },
+          {
+            Модель: "MacBook Pro 16",
+            price: 250000,
+          },
+          {
+            Модель: "MacBook Pro 19",
+            price: 550000,
+          },
+        ],
+
+        Dell: [
+          {
+            Модель: "Spirition 15",
+            price: 44230,
+          },
+          {
+            Модель: "Lampitude 13",
+            price: 57000,
+          },
+          {
+            Модель: "XYZ 15",
+            price: 199000,
+          },
+        ],
+      },
+    },
+  },
+};
