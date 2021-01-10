d346279af27baf48f062a7d39f70df7987166865
odivus <odivus@gmail.com>
2021-01-10 19:23:17 +0300

    User data fin done

--- pages/user-work.tsx
+++ pages/user-work.tsx
@@ -90,21 +90,6 @@ function UserWork(props: UserDataProps) {
               labelText='Доход, &#8381;/мес.'
               handler={(e) => inputChange(e, inputValue, setInputValue)}
             />
-            {/* <div className='input-field'>
-              <input id='work' type='text' className='validate' />
-              <label htmlFor='work'>Место работы</label>
-            </div> */}
-
-            {/* <div className='input-field'>
-              <input id='appointment' type='text' className='validate' />
-              <label htmlFor='appointment'>Должность</label>
-            </div> */}
-
-            {/* <div className='input-field'>
-              <input id='income' type='text' className='validate' />
-              <label htmlFor='income'>Доход, &#8381;/мес.</label>
-            </div> */}
-
             <Button
               className='btn waves-effect waves-light block-centered btn-custom_submit btn-custom_green'
               type='submit'
