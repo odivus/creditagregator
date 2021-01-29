9e3d858305a15630134b39a05eedace379ba7736
odivus <odivus@gmail.com>
2021-01-29 22:47:53 +0300

    Goods Card add done

--- components/Goods-card/Goods-card.tsx
+++ components/Goods-card/Goods-card.tsx
@@ -1,21 +1,48 @@
+import {useState} from 'react';
+import Quantity from '../Quantity/Quantity';
+
 import cx from 'classnames';
 import styles from './Goods-card.module.scss';
 
 function GoodsCard(props) {
   console.log(props);
+  const {
+    category,
+    brand,
+    model,
+    price,
+    quantity,
+    index,
+    _id,
+  } = props;
+
+  const [cardQuantity, setCardQuantity] = useState(quantity);
+
   return (
     <div className={cx(styles['card-wrapper'], 'flex-centered')}>
       <i className={cx('small', 'material-icons', styles.close)}>close</i>
       <div className={cx(styles['goods-card'], 'flex-centered')}>
         <div className={cx(styles['goods-card__item'], 'flex-centered')}>
-          <div className={styles['goods-card__number']}>99</div>
-          {/* <GoodsSelect {...props} /> */}
+          <div className={styles['goods-card__number']}>{index}</div>
+          <div className={styles['goods-card__content']}>
+            <div>
+              Категория: <p>{category}</p>
+            </div>
+            <div>
+              Бренд: <p>{brand}</p>
+            </div>
+            <div>
+              Модель: <p>{model}</p>
+            </div>
+          </div>
         </div>
-        {/* <Quantity /> */}
+        <Quantity quantity={cardQuantity} setQuantity={setCardQuantity} />
       </div>
       <div className={styles['goods-card__cost']}>
         Цена&nbsp;
-        <span className={styles['cost-number']}>120000&nbsp;&#8381;</span>
+        <span className={styles['cost-number']}>
+          {price * cardQuantity}&nbsp;&#8381;
+        </span>
       </div>
     </div>
   );
--- components/Goods/Goods.tsx
+++ components/Goods/Goods.tsx
@@ -9,15 +9,56 @@ import styles from './Goods.module.scss';
 function Goods(props) {
   const { selectedFullModel } = props;
   const [quantity, setQuantity] = useState(1);
+  const [goodsAdded, setGoodsAdded] = useState([]);
 
-  const changedPropsQuantity = {
-    ...selectedFullModel,
-    quantity: quantity,
-  };
+  function showGoodsCards() {
+    return goodsAdded
+      .map((goodsItem, index) => (
+        <GoodsCard 
+          key={goodsItem._id} 
+          {
+            ...{
+              index: ++index, 
+              ...goodsItem,
+            }
+          } 
+        />
+      ))
+      .reverse();
+  }
+
+  function addGoods() {
+    const findDuplicate = goodsAdded.find(
+      (item) => item._id === selectedFullModel._id
+    );
+    // const findDuplicate = goodsAdded.find(
+    //   item => (item._id === selectedFullModel._id) && 
+    //           (item.quantity === quantity)
+    // );
+
+    const addedItem = [
+      ...goodsAdded,
+      {
+        ...selectedFullModel,
+        quantity: quantity,
+      },
+    ];
+
+    if (findDuplicate) return;
+    // console.log(addedItem);
+
+    // const itemWithMaxQuantity = addedItem.reduce((current, next) => {
+    //   if (next.quantity > current.quantity) return next;
+    // });
+
+    // console.log(itemWithMaxQuantity);
+
+    setGoodsAdded(addedItem);
+  }
+
+  console.log(goodsAdded);
   // console.log(selectedFullModel);
   // console.log(changedPropsQuantity);
-
-  // if (quantity > 1) selectedModel.quantity = quantity;
 
   return (
     <div className='row row_content'>
@@ -27,7 +68,13 @@ function Goods(props) {
           <Quantity quantity={quantity} setQuantity={setQuantity} />
         </div>
         <div className={cx(styles['goods-buttons'], 'flex-centered')}>
-          <button className='waves-effect waves-light btn btn-large-custom btn-custom_blue'>
+          <button
+            className='waves-effect waves-light btn btn-large-custom btn-custom_blue'
+            onClick={() => {
+              addGoods();
+              setQuantity(1);
+            }}
+          >
             Добавить товар
           </button>
           <button className='waves-effect waves-light btn btn-large-custom btn-custom_green'>
@@ -49,7 +96,8 @@ function Goods(props) {
       </div>
       <div className={cx('col', 's12', 'm7', 'l7', styles.goods)}>
         <div className={cx(styles['goods-wrapper'], 'custom-scroll-bar')}>
-          <GoodsCard {...changedPropsQuantity} />
+          {showGoodsCards()}
+          {/* <GoodsCard {...goodsAdded} /> */}
           {/* <GoodsCard {...props} />
           <GoodsCard {...props} />
           <GoodsCard {...props} />
