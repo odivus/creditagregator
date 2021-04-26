163fedfb271a0d936e457db8aa5e888a4245f569
odivus <odivus@gmail.com>
2021-04-26 14:16:23 +0300

    Logo styles mod

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -7,10 +7,10 @@ import styles from './Logo.module.scss';
 function Logo() {
   const title = 'Unicredit — прототип сервиса по подбору кредитов';
   const documentTitle = useGetDocumentTitle();
-  const className = cx({
-    'logo-active': documentTitle !== title,
+  const logoClassName = cx({
+    [styles.logo]: true,
+    [styles['logo_active']]: documentTitle !== title,
   });
-  const logoClassName = `${styles.logo} ${styles.header__logo} ${styles[className]}`;
   
   return ( 
     documentTitle !== title
